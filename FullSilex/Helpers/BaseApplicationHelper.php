ce1275f963dd6ae0053b61957781c2e55f23682d
Bobby Stenly <iceman.bsi@gmail.com>
2016-11-19 13:42:15 +0700

    BaseApplicationHelper

--- FullSilex/Helpers/BaseApplicationHelper.php
+++ FullSilex/Helpers/BaseApplicationHelper.php
@@ -13,6 +13,10 @@ class BaseApplicationHelper
 {
     protected $app;
 
+    public function  __construct($app) {
+        $this->app = $app;
+    }
+
     public function setApp($app){
         $this->app = $app;
     }
