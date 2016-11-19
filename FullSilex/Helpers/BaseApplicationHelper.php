189a76f1c5ba76b74f4c0d2fb9999e03ac9e33b2
Bobby Stenly <iceman.bsi@gmail.com>
2016-11-19 13:39:16 +0700

    BaseApplicationHelper

---
+++ FullSilex/Helpers/BaseApplicationHelper.php
@@ -0,0 +1,19 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: bobbystenlyirawan
+ * Date: 11/19/16
+ * Time: 1:33 PM
+ */
+
+namespace FullSilex\Helpers;
+
+
+class BaseApplicationHelper
+{
+    protected $app;
+
+    public function setApp($app){
+        $this->app = $app;
+    }
+}
